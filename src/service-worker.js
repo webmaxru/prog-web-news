f845123cd4e11895b3f514c3b7724313d9f0d7a2
Maxim Salnikov <salnikov@gmail.com>
2021-04-06 23:16:50 +0200

    sw

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,4 +1,69 @@
-import { precacheAndRoute } from "workbox-precaching";
+import { precacheAndRoute, createHandlerBoundToURL } from "workbox-precaching";
+import { NavigationRoute, registerRoute } from "workbox-routing";
+import { setCacheNameDetails, clientsClaim } from "workbox-core";
+import {
+  CacheFirst,
+  NetworkFirst,
+  StaleWhileRevalidate,
+} from "workbox-strategies";
+import { ExpirationPlugin } from "workbox-expiration";
+import { CacheableResponsePlugin } from "workbox-cacheable-response";
+
+// SETTINGS
+
+// Claiming control to start runtime caching asap
+clientsClaim();
+
+// Use to update the app after user triggered refresh
+self.skipWaiting();
+
+// Setting custom cache names
+setCacheNameDetails({ precache: "wb6-precache", runtime: "wb6-runtime" });
+
+// PRECACHING
 
 // Precache and serve resources from __WB_MANIFEST array
 precacheAndRoute(self.__WB_MANIFEST);
+
+// NAVIGATION ROUTING
+
+// This assumes /index.html has been precached.
+const navHandler = createHandlerBoundToURL("/index.html");
+const navigationRoute = new NavigationRoute(navHandler, {
+  denylist: [new RegExp("/out-of-spa/")], // Also might be specified explicitly via allowlist
+});
+registerRoute(navigationRoute);
+
+// RUNTIME CACHING
+
+// Load details immediately and check for update right after
+registerRoute(
+  new RegExp("https://progwebnews-app.azurewebsites.net.*content/posts/slug.*"),
+  new StaleWhileRevalidate({
+    cacheName: "wb6-post",
+    plugins: [
+      new ExpirationPlugin({
+        // Only cache requests for a week
+        maxAgeSeconds: 7 * 24 * 60 * 60,
+      }),
+    ],
+  })
+);
+
+// Keeping lists always fresh
+registerRoute(
+  new RegExp("https://progwebnews-app.azurewebsites.net.*content/posts.*"),
+  new NetworkFirst()
+);
+
+// Avatars can live in cache
+registerRoute(
+  ({ url }) => url.hostname.includes("gravatar.com"),
+  new CacheFirst({
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
