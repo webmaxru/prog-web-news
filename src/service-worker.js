8f3ac629809f8079e902438ab835d51be09ddedd
Maxim Salnikov <salnikov@gmail.com>
2021-04-06 23:09:24 +0200

    sw

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,4 +1,28 @@
-import { precacheAndRoute } from "workbox-precaching";
+import { precacheAndRoute, createHandlerBoundToURL } from "workbox-precaching";
+import { NavigationRoute, registerRoute } from "workbox-routing";
+import { setCacheNameDetails, clientsClaim } from "workbox-core";
+
+// SETTINGS
+
+// Claiming control to start runtime caching asap
+clientsClaim();
+
+// Use to update the app after user triggered refresh
+self.skipWaiting();
+
+// Setting custom cache names
+setCacheNameDetails({ precache: "wb6-precache", runtime: "wb6-runtime" });
+
+// PRECACHING
 
 // Precache and serve resources from __WB_MANIFEST array
 precacheAndRoute(self.__WB_MANIFEST);
+
+// NAVIGATION ROUTING
+
+// This assumes /index.html has been precached.
+const navHandler = createHandlerBoundToURL("/index.html");
+const navigationRoute = new NavigationRoute(navHandler, {
+  denylist: [new RegExp("/out-of-spa/")], // Also might be specified explicitly via allowlist
+});
+registerRoute(navigationRoute);
